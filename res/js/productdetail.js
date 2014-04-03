b269da7ba7999f384c8fa79dfa38d459cdc6301b
zhanyufengg <zhanyufeng1218@163.com>
2014-04-03 20:28:49 +0800

    加入实验代码

--- res/js/productdetail.js
+++ res/js/productdetail.js
@@ -38,6 +38,78 @@ $(window).load(function(e) {
     main_img.attr({src : "./data/" + _data.main_img});
     main_content_title.text(_data.main_content_title);
     main_content_dec.text(_data.main_content_dec);
-    titlebar.title.text({src : _data.main_content_title});
+    titlebar.title.text(_data.main_content_title);
+
+
+
+
+    var Item = function(){
+        var item_content_title = $("<h2/>");
+        var item_content_divide = $("<img />")
+            .attr("src", "res/drawables/product_detail_item_divider.jpg")
+            .css({
+                "margin-top": 7+"px",
+                "margin-bottom": 7+"px",
+                "margin-left": 2+"px"
+            });
+        var item_content_dec = $("<p/>")
+            .css({
+                "width" : 130+"px",
+                "height" : 90+"px"
+            });
+        var item_content_more = $("<img/>")
+            .attr("src", "res/drawables/product_detail_item_more.png")
+            .css({
+                "float" : "right"
+            });
+        var item_content_right = $("<div/>")
+            .css({
+                "float" : "left",
+                "width" : 150+"px",
+                "height" : 143+"px",
+                "margin-left" : 20+"px",
+                "padding-top" : 10+"px"
+            });
+        $(item_content_right).append(item_content_title,item_content_divide,item_content_dec,item_content_more);
+        var item_content_img = $("<img/>")
+            .css({
+                "float" : "right"
+            });
+        var item_content = $("<div/>")
+            .css({
+                "position" : "absolute",
+                "top" : 0+"px",
+                "left" : 0+"px",
+                "width" : 402+"px",
+                "padding" : 10+"px"
+            });
+        $(item_content).append(item_content_img,item_content_right);
+        var item_bg = $("<img/>")
+            .attr("src", "res/drawables/product_detail_item_bg.png")
+            .css({
+                "position" : "absolute",
+                "top" : 0+"px",
+                "left" : 0+"px"
+            });
+        var item = $("<div/>")
+            .css({
+                "position" : "relative",
+                "top" : 0+"px",
+                "left" : 0+"px",
+                "width" : 402+"px",
+                "height" : 171+"px",
+                "float" : "left",
+                "margin-right" : 40+"px"
+            });
+        $(item).append(item_bg).append(item_content);
+    }
+
+    var item = new Item();
+    item.item_content_title.text("这是标题");
+    item.item_content_img.attr({src : "res/drawables/product_detail_item_image.png"});
+    item.item_content_dec.text("这是内容aaaaaaaaaaaaaaaaa");
+
+    var produce_detail = $("#produce_detail");
+    $("#row").append(item.item);
 });
 
